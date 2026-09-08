381e063059b81f7e9ac9647fce80aba8baf72104
wieslawpaleta <tankluk@gmail.com>
2026-09-08 21:31:29 +0200

    Add files via upload

--- praktyka.py
+++ praktyka.py
@@ -1,6 +1,7 @@
 #Praktyka W3Schools
-#Python Introduction, Getting Started, Syntax, Statements, Syntax Code Challenge, Output / Print, Numbers, Output Code Challenge, Comments(tu jestem teraz)
-#
+#Python Introduction, Getting Started, Syntax, Statements, Syntax Code Challenge, Output / Print, Numbers, Output Code Challenge, Comments, Challenge: Comments,
+#Variables, Variable Names, Variables - Assign Multiple Values, Output Variables(Teraz tu jestem!)
+
 import sys
 
 
@@ -16,8 +17,12 @@ if x != y:
     print("\nHow are you?")
 
 
+print(x * y)
+
+
 print("I haven't known"); print("that"); print("it is possible!")
 print("Double quotes"); print("Single quotes")
+
 
 print("I have keys but open no locks.I have space but no room.You can enter, but you can't go outside. What am I?", end=" ")
 print("A computer keyboard.")
@@ -25,3 +30,61 @@ print(1)
 print("I am", 1000, "years old.")
 print(1 + 1000)
 
+
+"""
+This is a multiline comment,
+but you will not see it, right?
+
+"""
+
+
+z = str(3)
+w = int(3)
+u = float(3)
+
+
+print(type(z))
+print(type(w))
+print(type(u))
+
+
+zupa = "zupa"
+apuz = 'zupa'
+print(zupa + " = " + apuz)
+
+
+p = "rower"
+P = 0
+print(p)
+print(P)
+
+
+myvar = "Grzyb"
+my_var = "Grzyb"
+_my_var = "Grzyb"
+myVar = "Grzyb"
+MYVAR = "Grzyb"
+myvar2 = "Grzyb"
+myVarName = "Grzyb"
+MyVarName = "Grzyb"
+my_var_name = "Grzyb"
+myvar = "Grzyb"
+
+
+x, y, z = "Castle", "Stronghold", "Fortress"
+print(x)
+print(y)
+print(z)
+
+
+x = y = z = "Citadel"
+print(x)
+print(y)
+print(z)
+
+
+buildings = ["Castle", "Stronghold", "Fortress"]
+x, y, z = buildings
+print(x)
+print(y)
+print(z)

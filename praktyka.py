d7000fb4cacc204d7234229d056802146da07823
wieslawpaleta <tankluk@gmail.com>
2026-09-13 20:45:55 +0200

    Add files via upload

--- praktyka.py
+++ praktyka.py
@@ -290,7 +290,8 @@ print(b.replace("U", "A"))
 
 print(b.split(" "))
 
-#  String Concatenation, Format - Strings, Escape Characters, String Methods  (Teraz tu jestem!)
+#  String Concatenation, Format - Strings, Escape Characters, String Methods, String Exercises, Strings Code Challenge, Booleans
+#   (I am here now!)
 #
 #
 #
@@ -321,3 +322,38 @@ print(txt)
 
 txt = "We\t are\b\rso-called \"Vikings\" \\from\n the\f north." 
 print(txt)
+
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+
+print(bool("Hello"))
+print(bool(15))
+print(bool(0))
+
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
